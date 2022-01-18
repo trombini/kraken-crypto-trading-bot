7a3175f145afa9d1801aa7b492ec04f0c3237b4f
trombini <riccardo@trombini.ch>
2022-01-18 17:38:38 +0100

    more logging

--- src/indicators/rsi/rsi.ts
+++ src/indicators/rsi/rsi.ts
@@ -13,10 +13,11 @@ export const rsi = (name: string) => (blocks: OHLCBlock[]): number  => {
     values: ohlc.close
   })
 
+  const threshold = 40
   const rsi = takeRight(history, 1)[0]
-  const confidence = rsi < 40 ? 1 : 0
+  const confidence = rsi < threshold ? 1 : 0
 
-  logger.debug(`RSI (${name}): [ ${round(rsi, 2)} ] => ${confidence}`)
+  logger.debug(`RSI (${name}) - ${round(rsi, 2)} < ${threshold} => ${confidence}`)
 
   return confidence
 }
